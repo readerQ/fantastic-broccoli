94bfcc6354bc9029c98e469ea1b36e038d1af4cd
Симаков Алексей <simakovaa@dalimo.ru>
2023-07-11 15:57:58 +0700

    init

---
+++ src/main/java/org/dalimo/rpa/tg/RpaTgApplication.java
@@ -0,0 +1,13 @@
+package org.dalimo.rpa.tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpaTgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpaTgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dalimo/rpa/tg/RpaTgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dalimo.rpa.tg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpaTgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
